0dffe4d19fd43106b4848873b267120bd3e6663b
Winston Ta <winstonnta@gmail.com>
2025-10-25 02:48:57 -0700

    new baseline react project

--- spark-cs/src/pages/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="container">
-      <h1>Contact Us</h1>
-      <p>Have questions? Reach out at <a href="mailto:team@sparkcs.example">team@sparkcs.example</a>.</p>
-    </div>
-  )
-}
